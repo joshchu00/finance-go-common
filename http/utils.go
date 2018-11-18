28183c4177cf0f808ebe5f71022bc2ddee9f582e
Josh Chu <>
2018-11-18 10:00:05 +0800

    Add http

---
+++ http/utils.go
@@ -0,0 +1,37 @@
+package http
+
+import (
+	"errors"
+	"io/ioutil"
+	"net/http"
+)
+
+func Get(url string) (bytes []byte, err error) {
+
+	client := &http.Client{}
+
+	var request *http.Request
+	request, err = http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		return
+	}
+
+	var response *http.Response
+	response, err = client.Do(request)
+	if err != nil {
+		return
+	}
+	defer response.Body.Close()
+
+	bytes, err = ioutil.ReadAll(response.Body)
+	if err != nil {
+		return
+	}
+
+	if 200 != response.StatusCode {
+		err = errors.New("Not 200")
+		return
+	}
+
+	return
+}
