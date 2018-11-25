7a059b16b35b30dc53afc960d688221c6b7cf6ab
Josh Chu <>
2018-11-25 17:14:30 +0800

    Fix bug

--- decimal/utils.go
+++ decimal/utils.go
@@ -7,6 +7,7 @@ import (
 )
 
 func GetDecimal(s string) (d *inf.Dec, err error) {
+	d = new(inf.Dec)
 	var success bool
 	if d, success = d.SetString(s); !success {
 		err = errors.New("Set string to decimal failed")
