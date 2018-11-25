9ac8f6e38343808d91331095d02275899f38799b
Josh Chu <>
2018-11-25 17:01:48 +0800

    Add datetime GetTime()

--- datetime/utils.go
+++ datetime/utils.go
@@ -8,6 +8,10 @@ func GetMilliSecond(t time.Time) int64 {
 	return t.UnixNano() / 1e6
 }
 
+func GetTime(ms int64) time.Time {
+	return time.Unix(ms/1e3, ms%1e3*1e6)
+}
+
 func GetDateString(t time.Time) string {
 	return t.Format("20060102")
 }
