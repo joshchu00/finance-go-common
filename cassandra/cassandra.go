56315be80628fb700e07888d7dce5ed0ca7e1959
Josh Chu <>
2018-11-25 17:00:24 +0800

    Add cassandra

---
+++ cassandra/cassandra.go
@@ -0,0 +1,59 @@
+package cassandra
+
+import (
+	"time"
+
+	"github.com/gocql/gocql"
+	inf "gopkg.in/inf.v0"
+)
+
+type Client struct {
+	cluster *gocql.ClusterConfig
+	session *gocql.Session
+}
+
+func NewClient(hosts string, keyspace string) (c *Client, err error) {
+
+	c = &Client{}
+
+	c.cluster = gocql.NewCluster(hosts)
+	c.cluster.Keyspace = keyspace
+	c.cluster.Consistency = gocql.Quorum
+
+	c.session, err = c.cluster.CreateSession()
+
+	return
+}
+
+func (c *Client) InsertRecord(
+	exchange string,
+	symbol string,
+	period string,
+	datetime time.Time,
+	open *inf.Dec,
+	high *inf.Dec,
+	low *inf.Dec,
+	close *inf.Dec,
+	volume int64,
+) (err error) {
+
+	r := &record{
+		exchange,
+		symbol,
+		period,
+		datetime,
+		open,
+		high,
+		low,
+		close,
+		volume,
+	}
+
+	err = r.insert(c.session)
+
+	return
+}
+
+func (c *Client) Close() {
+	c.session.Close()
+}
---
+++ cassandra/record.go
@@ -0,0 +1,42 @@
+package cassandra
+
+import (
+	"time"
+
+	"github.com/gocql/gocql"
+	inf "gopkg.in/inf.v0"
+)
+
+type record struct {
+	exchange string
+	symbol   string
+	period   string
+	datetime time.Time
+	open     *inf.Dec
+	high     *inf.Dec
+	low      *inf.Dec
+	close    *inf.Dec
+	volume   int64
+}
+
+var (
+	insertRecordCQL = "INSERT INTO record (exchange, symbol, period, datetime, open, high, low, close, volume) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)"
+)
+
+func (r *record) insert(session *gocql.Session) (err error) {
+
+	err = session.Query(
+		insertRecordCQL,
+		r.exchange,
+		r.symbol,
+		r.period,
+		r.datetime,
+		r.open,
+		r.high,
+		r.low,
+		r.close,
+		r.volume,
+	).Exec()
+
+	return
+}
