e168fa3f36654961b1f569c41f7479facc6fd792
Josh Chu <>
2018-12-09 13:05:53 +0800

    Change time base to timestamp base

--- datetime/utils.go
+++ datetime/utils.go
@@ -4,14 +4,18 @@ import (
 	"time"
 )
 
-func GetMilliSecond(t time.Time) int64 {
+func GetTimestamp(t time.Time) int64 {
 	return t.UnixNano() / 1e6
 }
 
-func GetTime(ms int64) time.Time {
-	return time.Unix(ms/1e3, ms%1e3*1e6)
+func getTime(ts int64) time.Time {
+	return time.Unix(ts/1e3, ts%1e3*1e6)
 }
 
-func GetDateString(t time.Time) string {
-	return t.Format("20060102")
+func GetTimestampString(ts int64) string {
+	return getTime(ts).String()
+}
+
+func GetDateString(ts int64) string {
+	return getTime(ts).Format("20060102")
 }
