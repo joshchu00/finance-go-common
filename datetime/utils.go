1058914d0a2e9259220a12f3331149dd149d31c5
Josh Chu <>
2018-12-09 13:33:00 +0800

    Add location

--- datetime/utils.go
+++ datetime/utils.go
@@ -8,14 +8,14 @@ func GetTimestamp(t time.Time) int64 {
 	return t.UnixNano() / 1e6
 }
 
-func getTime(ts int64) time.Time {
-	return time.Unix(ts/1e3, ts%1e3*1e6)
+func getTime(ts int64, loc *time.Location) time.Time {
+	return time.Unix(ts/1e3, ts%1e3*1e6).In(loc)
 }
 
-func GetTimeString(ts int64) string {
-	return getTime(ts).String()
+func GetTimeString(ts int64, loc *time.Location) string {
+	return getTime(ts, loc).String()
 }
 
-func GetDateString(ts int64) string {
-	return getTime(ts).Format("20060102")
+func GetDateString(ts int64, loc *time.Location) string {
+	return getTime(ts, loc).Format("20060102")
 }
