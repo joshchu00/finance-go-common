0dd6d73cf40e8ab9bbee171893672a65c37807d1
Josh Chu <>
2019-01-06 16:45:01 +0800

    Add SelectAllRecord

--- cassandra/cassandra.go
+++ cassandra/cassandra.go
@@ -1,10 +1,7 @@
 package cassandra
 
 import (
-	"time"
-
 	"github.com/gocql/gocql"
-	inf "gopkg.in/inf.v0"
 )
 
 type Client struct {
@@ -21,37 +18,6 @@ func NewClient(hosts string, keyspace string) (c *Client, err error) {
 	c.cluster.Consistency = gocql.Quorum
 
 	c.session, err = c.cluster.CreateSession()
-
-	return
-}
-
-func (c *Client) InsertRecord(
-	exchange string,
-	symbol string,
-	period string,
-	datetime time.Time,
-	name string,
-	open *inf.Dec,
-	high *inf.Dec,
-	low *inf.Dec,
-	close *inf.Dec,
-	volume int64,
-) (err error) {
-
-	r := &record{
-		exchange,
-		symbol,
-		period,
-		datetime,
-		name,
-		open,
-		high,
-		low,
-		close,
-		volume,
-	}
-
-	err = r.insert(c.session)
 
 	return
 }
--- cassandra/record.go
+++ cassandra/record.go
@@ -3,11 +3,10 @@ package cassandra
 import (
 	"time"
 
-	"github.com/gocql/gocql"
 	inf "gopkg.in/inf.v0"
 )
 
-type record struct {
+type Record struct {
 	exchange string
 	symbol   string
 	period   string
@@ -20,14 +19,12 @@ type record struct {
 	volume   int64
 }
 
-var (
-	insertRecordCQL = "INSERT INTO record (exchange, symbol, period, datetime, name, open, high, low, close, volume) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
-)
+func (c *Client) InsertRecord(r *Record) (err error) {
 
-func (r *record) insert(session *gocql.Session) (err error) {
+	cql := "INSERT INTO record (exchange, symbol, period, datetime, name, open, high, low, close, volume) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 
-	err = session.Query(
-		insertRecordCQL,
+	err = c.session.Query(
+		cql,
 		r.exchange,
 		r.symbol,
 		r.period,
@@ -39,6 +36,50 @@ func (r *record) insert(session *gocql.Session) (err error) {
 		r.close,
 		r.volume,
 	).Exec()
+
+	return
+}
+
+func (c *Client) SelectAllRecord(exchange string, symbol string, period string) (rs []*Record, err error) {
+
+	cql := "SELECT exchange, symbol, period, datetime, name, open, high, low, close, volume FROM record WHERE exchange = ? AND symbol = ? AND period = ? ORDER BY datetime ASC"
+
+	iter := c.session.Query(
+		cql,
+		exchange,
+		symbol,
+		period,
+	).Iter()
+
+	var datetime time.Time
+	var name string
+	var open, high, low, close *inf.Dec
+	var volume int64
+
+	rs = make([]*Record, 0)
+
+	for iter.Scan(&exchange, &symbol, &period, &datetime, &name, &open, &high, &low, &close, &volume) {
+		rs = append(
+			rs,
+			&Record{
+				exchange: exchange,
+				symbol:   symbol,
+				period:   period,
+				datetime: datetime,
+				name:     name,
+				open:     open,
+				high:     high,
+				low:      low,
+				close:    close,
+				volume:   volume,
+			},
+		)
+	}
+
+	err = iter.Close()
+	if err != nil {
+		return
+	}
 
 	return
 }
